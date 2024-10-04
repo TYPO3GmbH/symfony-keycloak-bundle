b10918e583f02106a86c571e7d34fdd30c134b15
Marvin Buchmann <marvin.buchmann@typo3.com>
2024-10-04 14:17:39 +0200

    Add test

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3g/symfony-keycloak-bundle.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+require dirname(__DIR__) . '/vendor/autoload.php';
