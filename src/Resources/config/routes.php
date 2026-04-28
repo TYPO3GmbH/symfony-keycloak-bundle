47e68989f298cad13b7268002dfc139a69c830cb
Andreas Kienast <andreas.kienast@typo3.com>
2026-04-28 11:18:27 +0200

    [TASK] Add routes.php as drop-in replacement for routes.xml

---
+++ src/Resources/config/routes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Routing\Loader\XmlFileLoader;
+
+return function (RoutingConfigurator $routes): void {
+    foreach (debug_backtrace() as $trace) {
+        if (isset($trace['object']) && $trace['object'] instanceof XmlFileLoader && 'doImport' === $trace['function']) {
+            if (__DIR__ === dirname(realpath($trace['args'][3]))) {
+                trigger_deprecation('t3g/symfony-keycloak-bundle', '4.0', 'The "routes.xml" routing configuration file is deprecated, import "routes.php" instead.');
+
+                break;
+            }
+        }
+    }
+
+    $routes->add('t3g_keycloak_login', '/login')
+        ->controller('keycloak.typo3.com.login_controller::login')
+    ;
+    $routes->add('t3g_keycloak_oauthCallback', '/oauth/callback}')
+        ->controller('keycloak.typo3.com.login_controller::oauthCallback')
+    ;
+    $routes->add('t3g_keycloak_logout', '/oauth/logout}')
+        ->controller('keycloak.typo3.com.login_controller::oauthLogout')
+    ;
+};
