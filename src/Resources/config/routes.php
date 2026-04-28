5b7c8bfa53c449d897d47e733912d60ebdcf0a93
Andreas Kienast <andreas.kienast@typo3.com>
2026-04-28 11:21:19 +0200

    [BUGFIX] Fix CGL

--- src/Resources/config/routes.php
+++ src/Resources/config/routes.php
@@ -1,12 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the package t3g/symfony-keycloak-bundle.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
 
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
